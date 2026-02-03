fe162b012a0deb05f4abdc7e9cf101d6ca1b8fda
Dipak Prasad <dipakprasad68@gmail.com>
2026-02-03 19:51:37 +0530

    Updates app logic

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,36 +1,17 @@
 package com.example.demo;
 
-import com.example.demo.entity.User;
-import com.example.demo.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
 
-    private final UserRepository userRepository;
-
-    public DemoApplication(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
     @GetMapping("/hello")
     public String hello() {
-        return "Hello from AWS 3-Tier Java Application with RDS 🚀";
-    }
-
-    @PostMapping("/users")
-    public User createUser(@RequestBody User user) {
-        return userRepository.save(user);
-    }
-
-    @GetMapping("/users")
-    public List<User> getUsers() {
-        return userRepository.findAll();
+        return "Hello from AWS 3-Tier Java Application 🚀";
     }
 
     @GetMapping("/health")
